8209782f6afb232e9d29282544a851e536a560b8
Darshan Kudache <156560377+Dachhu7@users.noreply.github.com>
2024-09-05 19:29:28 +0530

    ProductItem.js

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/ProductItem.css";
 
-const ProductItem = ({ product, onAddToCart }) => {
+const ProductItem = ({ product }) => {
   return (
     <div className="product-item">
-      <img src={product.image} alt={product.title} />
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <p>Original Price: {product.originalPrice}</p>
-      <p>Offer Price: {product.offerPrice}</p>
-      <button onClick={() => onAddToCart(product)}>Add to Cart</button>
-      <Link to={`/products/${product.id}`}>View Details</Link>
+      <div className="card text-center">
+        <img src={product.image} alt={product.title} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{product.title.substring(0, 68)}</h5>
+          <p className="card-text">{product.price}</p> {/* Display price in rupees */}
+          <div className="buttons">
+            <Link
+              to={`/products/${product.id}`}
+              className="btn btn-outline-dark"
+            >
+              Buy Now
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
