8326a70257b9d1fe4ca2e304a102804733a9ddca
Darshan Kudache <156560377+Dachhu7@users.noreply.github.com>
2024-09-05 23:22:10 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import CartContextProvider from './context/CartContextProvider';
+import { CartContextProvider } from './context/CartContextProvider'; // Ensure this is correct
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <React.StrictMode>
     <CartContextProvider>
-    <App />
+      <App />
     </CartContextProvider>
   </React.StrictMode>
 );
-
-
